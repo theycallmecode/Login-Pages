69c047453a165f87be6ae664e7734907f93b0efb
Prism <ershubhcsofficial@gmail.com>
2025-04-09 18:21:27 +0530

    files changes

---
+++ 2. Travel Booking Login Page/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    function showNextSlide() {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }
+    slides[currentSlide].classList.add('active');
+    setInterval(showNextSlide, 3000);
+});
